c033b814fe76f9e78620dbf77b7f1b870f9def64
kitoyjami <eddyuga@gmail.com>
2022-07-28 16:50:03 -0500

    new project add

--- servidor_node/index.js
+++ servidor_node/index.js
@@ -7,4 +7,12 @@ const http = require('http')
 // SERVIDOR
 const server = http.createServer( (req,res) => {
 
+     console.log('req', req.headers.authorization)
+     console.log('req', req.url)
+     console.log('req', req.method)
+})
+
+
+server.listen(3000,'localhost', ()=>{
+    console.log('Servidor arriba');
 })
