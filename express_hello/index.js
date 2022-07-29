1a81f0a7493cca915bde70b64d9377494f4d0703
kitoyjami <eddyuga@gmail.com>
2022-07-28 19:04:20 -0500

    new params in the project

--- express_hello/index.js
+++ express_hello/index.js
@@ -5,17 +5,71 @@ import express from 'express'
 // guarda funciones o métodos para trabajar la req, res, middle, handles, routers
 const app = express()   
 
-// localhost:3001/
+// localhost:3001/ => ruta raiz => USERS
+
+/** SINTAXIS DE RESPUESTA DE NUESTRA API
+    * {
+    *  "message": "",
+    * "data": {}
+    * }
+*/
+
+// AQUI VAMOS A PONER NUESTROS MIDDLEWARES
+
+// express.json() // para que nosotros podamos recibir json
+
+app.use(express.json())
 
 // define el verbo HTTP con 2 params; la ruta '/' y el handler
-app.get('/', (req, res)=> {
-    console.log('res =========', res)
+app.get('/', (_, res)=> {
+    res.status(200).json({message: "all users", 
+    data:[
+        {
+            id:1, 
+            name: "juan",
+            lastName: "perez",
+            email: "jgarcia@crsleon.com"
+        },
+        {
+            id:2,
+            name: "Eddy",
+            lastName: "Urbano",
+            email: "eurbano@crsleon.com"
+        },
+    ],
+})
+})
 
-    res.send('primer backend con express')
+app.post('/', (req, res)=> {
+    res.status(201).json({
+        message: "user created successfully",
+        data: { name: req.body.name,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                id: 3 }, 
+    })
+
+})
+
+app.delete('/:id', (req, res)=> {
+    console.log('params', req.params.id)
+    res.status(200).json(
+        {
+            message: "user deleted successfully",
+            data: [
+                { }   
+            ]
+
+        }
+
+    )
+
+})
+
+app.put('/', (req, res)=> {
 })
 
 // Se define el puerto y el callback
 app.listen(3001, ()=> {
-
     console.log('Application running on port 3001')
 })
